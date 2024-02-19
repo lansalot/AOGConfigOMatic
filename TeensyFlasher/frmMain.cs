80c6dfebf424750e319356cef70a7ce226414711
lansalot <lansalot@gmail.com>
2024-02-19 23:30:21 +0000

    [MAJOR] (MAJOR} major trigger

--- TeensyFlasher/frmMain.cs
+++ TeensyFlasher/frmMain.cs
@@ -18,6 +18,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
+
 namespace TeensyFlasher
 {
     public partial class frmMain : Form
