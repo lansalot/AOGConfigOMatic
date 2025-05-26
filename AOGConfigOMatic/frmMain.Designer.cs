74050639b9e682de7ffca95a21e5fbfe188d7d29
FantasyTeddy <matthiasbaer@bluewin.ch>
2025-05-26 16:38:19 +0200

    Extract Teensy into custom UserControl

--- AOGConfigOMatic/frmMain.Designer.cs
+++ AOGConfigOMatic/frmMain.Designer.cs
@@ -56,15 +56,7 @@ namespace AOGConfigOMatic
             this.btnURefresh = new System.Windows.Forms.Button();
             this.lblUblox = new System.Windows.Forms.Label();
             this.tabTeensy = new System.Windows.Forms.TabPage();
-            this.lbTeensies = new System.Windows.Forms.ListBox();
-            this.pbProgram = new System.Windows.Forms.ProgressBar();
-            this.lblMessages = new System.Windows.Forms.Label();
-            this.txtMessages = new System.Windows.Forms.TextBox();
-            this.btnRefreshTeensy = new System.Windows.Forms.Button();
-            this.btnProgram = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lbFirmware = new System.Windows.Forms.ListBox();
+            this.teensyControl = new AOGConfigOMatic.Teensy.TeensyControl();
             this.tabUM982 = new System.Windows.Forms.TabPage();
             this.btnConfigUM982 = new System.Windows.Forms.Button();
             this.txtSerialChatUM982 = new System.Windows.Forms.TextBox();
@@ -289,15 +281,7 @@ namespace AOGConfigOMatic
             // 
             // tabTeensy
             // 
-            this.tabTeensy.Controls.Add(this.lbTeensies);
-            this.tabTeensy.Controls.Add(this.pbProgram);
-            this.tabTeensy.Controls.Add(this.lblMessages);
-            this.tabTeensy.Controls.Add(this.txtMessages);
-            this.tabTeensy.Controls.Add(this.btnRefreshTeensy);
-            this.tabTeensy.Controls.Add(this.btnProgram);
-            this.tabTeensy.Controls.Add(this.label2);
-            this.tabTeensy.Controls.Add(this.label1);
-            this.tabTeensy.Controls.Add(this.lbFirmware);
+            this.tabTeensy.Controls.Add(this.teensyControl);
             this.tabTeensy.Location = new System.Drawing.Point(4, 41);
             this.tabTeensy.Name = "tabTeensy";
             this.tabTeensy.Padding = new System.Windows.Forms.Padding(3);
@@ -306,103 +290,14 @@ namespace AOGConfigOMatic
             this.tabTeensy.Text = "Teensy";
             this.tabTeensy.UseVisualStyleBackColor = true;
             // 
-            // lbTeensies
+            // teensyControl
             // 
-            this.lbTeensies.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTeensies.FormattingEnabled = true;
-            this.lbTeensies.ItemHeight = 20;
-            this.lbTeensies.Location = new System.Drawing.Point(100, 232);
-            this.lbTeensies.Margin = new System.Windows.Forms.Padding(2);
-            this.lbTeensies.Name = "lbTeensies";
-            this.lbTeensies.Size = new System.Drawing.Size(357, 44);
-            this.lbTeensies.TabIndex = 19;
-            // 
-            // pbProgram
-            // 
-            this.pbProgram.Location = new System.Drawing.Point(533, 285);
-            this.pbProgram.Margin = new System.Windows.Forms.Padding(2);
-            this.pbProgram.Name = "pbProgram";
-            this.pbProgram.Size = new System.Drawing.Size(172, 19);
-            this.pbProgram.TabIndex = 18;
-            // 
-            // lblMessages
-            // 
-            this.lblMessages.AutoSize = true;
-            this.lblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessages.Location = new System.Drawing.Point(10, 275);
-            this.lblMessages.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblMessages.Name = "lblMessages";
-            this.lblMessages.Size = new System.Drawing.Size(124, 29);
-            this.lblMessages.TabIndex = 17;
-            this.lblMessages.Text = "Messages";
-            // 
-            // txtMessages
-            // 
-            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMessages.Location = new System.Drawing.Point(14, 310);
-            this.txtMessages.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMessages.Multiline = true;
-            this.txtMessages.Name = "txtMessages";
-            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtMessages.Size = new System.Drawing.Size(692, 160);
-            this.txtMessages.TabIndex = 16;
-            // 
-            // btnRefreshTeensy
-            // 
-            this.btnRefreshTeensy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRefreshTeensy.Location = new System.Drawing.Point(600, 15);
-            this.btnRefreshTeensy.Margin = new System.Windows.Forms.Padding(2);
-            this.btnRefreshTeensy.Name = "btnRefreshTeensy";
-            this.btnRefreshTeensy.Size = new System.Drawing.Size(105, 42);
-            this.btnRefreshTeensy.TabIndex = 15;
-            this.btnRefreshTeensy.Text = "Refresh list";
-            this.btnRefreshTeensy.UseVisualStyleBackColor = true;
-            this.btnRefreshTeensy.Click += new System.EventHandler(this.btnRefreshTeensy_Click);
-            // 
-            // btnProgram
-            // 
-            this.btnProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProgram.Location = new System.Drawing.Point(600, 229);
-            this.btnProgram.Margin = new System.Windows.Forms.Padding(2);
-            this.btnProgram.Name = "btnProgram";
-            this.btnProgram.Size = new System.Drawing.Size(105, 37);
-            this.btnProgram.TabIndex = 14;
-            this.btnProgram.Text = "Program!";
-            this.btnProgram.UseVisualStyleBackColor = true;
-            this.btnProgram.Click += new System.EventHandler(this.btnProgramTeensy_Click);
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 237);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 36);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Teensy";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(5, 15);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(513, 29);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "1. Select a local firmware or Refresh list if none";
-            // 
-            // lbFirmware
-            // 
-            this.lbFirmware.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbFirmware.FormattingEnabled = true;
-            this.lbFirmware.ItemHeight = 20;
-            this.lbFirmware.Location = new System.Drawing.Point(16, 60);
-            this.lbFirmware.Margin = new System.Windows.Forms.Padding(2);
-            this.lbFirmware.Name = "lbFirmware";
-            this.lbFirmware.Size = new System.Drawing.Size(690, 164);
-            this.lbFirmware.TabIndex = 11;
-            this.lbFirmware.SelectedIndexChanged += new System.EventHandler(this.lbFirmware_SelectedIndexChanged_1);
+            this.teensyControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.teensyControl.Location = new System.Drawing.Point(0, 0);
+            this.teensyControl.Margin = new System.Windows.Forms.Padding(7);
+            this.teensyControl.Name = "teensyControl";
+            this.teensyControl.Size = new System.Drawing.Size(720, 539);
+            this.teensyControl.TabIndex = 14;
             // 
             // tabUM982
             // 
@@ -509,7 +404,6 @@ namespace AOGConfigOMatic
             this.pblF9PConfig.ResumeLayout(false);
             this.pblF9PConfig.PerformLayout();
             this.tabTeensy.ResumeLayout(false);
-            this.tabTeensy.PerformLayout();
             this.tabUM982.ResumeLayout(false);
             this.tabUM982.PerformLayout();
             this.ResumeLayout(false);
@@ -520,15 +414,6 @@ namespace AOGConfigOMatic
         private TabControl tbPages;
         private TabPage tabTeensy;
         private TabPage tabGPS;
-        private ListBox lbTeensies;
-        private ProgressBar pbProgram;
-        private Label lblMessages;
-        private TextBox txtMessages;
-        private Button btnRefreshTeensy;
-        private Button btnProgram;
-        private Label label2;
-        private Label label1;
-        private ListBox lbFirmware;
         private Button btnURefresh;
         private Label lblUblox;
         private TextBox txtSerialChat;
@@ -551,6 +436,7 @@ namespace AOGConfigOMatic
         private Button btnURefreshUM982;
         private TextBox txtSerialChatUM982;
         private Button btnConfigUM982;
+        private Teensy.TeensyControl teensyControl;
     }
 }
 
