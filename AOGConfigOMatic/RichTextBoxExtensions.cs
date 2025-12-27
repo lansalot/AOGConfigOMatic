3743268a57d4aa80b80bf043ba5e2886d0002011
FantasyTeddy <matthiasbaer@bluewin.ch>
2025-12-27 14:55:08 +0100

    Replace temporary batch file with direct invocation of ubxfwupdate.exe

---
+++ AOGConfigOMatic/RichTextBoxExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AOGConfigOMatic
+{
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox richTextBox, string text, Color color)
+        {
+            richTextBox.SelectionStart = richTextBox.TextLength;
+            richTextBox.SelectionLength = 0;
+            richTextBox.SelectionColor = color;
+            richTextBox.AppendText(text);
+            richTextBox.SelectionColor = richTextBox.ForeColor;
+        }
+    }
+}
